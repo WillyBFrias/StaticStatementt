990dace3f63a61e46b1b942a593c3d14a8d5c3bb
gbacio <gbacio@expertdata.com.mx>
2024-10-09 20:19:04 -0600

    Agregar archivos de proyecto.

---
+++ Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StaticStatementt
+{
+    internal class Student
+    {
+        public static int StudentNumber { get; set; }
+        public static void getAddNote()
+        {
+            MessageBox.Show("Static");
+        }
+    }
+}
